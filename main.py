95b93631a1e2704f3d012661655e0402ff3c1d51
f-do-bit <58906850+f-do-bit@users.noreply.github.com>
2026-02-25 22:09:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("===== SISTEMA DE TAREFAS =====")
+print("1 - Adicionar tarefa")
+print("2 - listar tarefas")
+print("3 - sair")
+
+opcao = input("Escolha uma opção: " )
+
+print("Você escolheu:", opcao)
